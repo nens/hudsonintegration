d5245468af75222907b352d00abe1b01fe54f777
reinout <reinout@vanrees.org>
2010-07-30 19:25:44 +0000

    created with nensskel

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import os.path
+
+version = '0.1dev'
+
+long_description = '\n\n'.join([
+    open('README.txt').read(),
+    open(os.path.join('hudsonintegration', 'USAGE.txt')).read(),
+    open('TODO.txt').read(),
+    open('CREDITS.txt').read(),
+    open('CHANGES.txt').read(),
+    ])
+
+install_requires = [
+    'setuptools',
+    ],
+
+tests_require = [
+    ]
+
+setup(name='hudsonintegration',
+      version=version,
+      description="TODO",
+      long_description=long_description,
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
+      keywords=[],
+      author='TODO',
+      author_email='TODO@nelen-schuurmans.nl',
+      url='',
+      license='GPL',
+      packages=['hudsonintegration'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require = {'test': tests_require},
+      entry_points={
+          'console_scripts': [
+          ]},
+      )
